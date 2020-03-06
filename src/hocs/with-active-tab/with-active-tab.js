6e29e1a3f9f8af3d6fa59efdcd4c5fe45ccb346f
Кирилл Черепанов <ktcherepanov@nologostudio.ru>
2020-03-07 01:16:08 +0300

    Поправил ошибку в импорте библиотеке в компоненте withActiveTab

--- src/hocs/with-active-tab/with-active-tab.js
+++ src/hocs/with-active-tab/with-active-tab.js
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'React';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
 import TabItem from '../../components/tab-item/tab-item.jsx';
